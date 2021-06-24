7cd45cec09619bc71da97280288df7f508bdb0e6
saurabhkawade784 <saurabhkawade784@gmail.com>
2021-06-24 14:39:41 +0530

    Flip coin multiple time.

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,13 +2,25 @@
 
 Head=1
 Tail=0
+headCount=0
+tailCount=0
 
 echo "Let's toss......"
-flipCoin=$((RANDOM%2))
 
-if [[ $Head -eq $flipCoin ]]
-then
-	echo "It's head..."
-else
-	echo "It's Tail..."
-fi
+read -p "Enter no of time you want to flip coin " noOfTime
+
+for (( i=1;i<=$noOfTime;i++ ))
+do
+	flipCoin=$((RANDOM%2))
+
+	if [[ $Head -eq $flipCoin ]]
+	then
+		echo "It's head..."
+		(( headCount++ ))
+	else
+		echo "It's Tail..."
+		(( tailCount++ ))
+	fi
+done
+echo "No of time head : " $headCount
+echo "No of time tail : " $tailCount 
